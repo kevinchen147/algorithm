dfc1dfe730e703bf6931fd5c23f562e216c8076a
xxgj <xxgj@outlook.com>
2023-01-03 12:07:43 +0800

    feat: update cpp template

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,17 +1,5 @@
-#include <algorithm>
-#include <chrono>
-#include <functional>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 
 #ifdef XXGJ_DEBUG
 #include "debug.h"
@@ -23,11 +11,11 @@ class Solution {};
 
 #ifdef XXGJ_DEBUG
 int main() {
-    Solution s;
-    std::vector<int> v{};
-    std::vector<std::vector<int>> vv{{}};
-    auto ans = s;
-    dbg(ans);
+    // Solution s;
+    // vector<int> v{};
+    // vector<vector<int>> vv{{}};
+    // auto ans = s;
+    // dbg(ans);
 
     return 0;
 }
