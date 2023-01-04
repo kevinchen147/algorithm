143fb0e9373cae1253a0d72256800861f38f3ac2
xxgj <xxgj@outlook.com>
2023-01-04 17:09:51 +0800

    feat: update cpp template

--- template/dijkstra.cpp
+++ template/dijkstra.cpp
@@ -1,71 +1,80 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-template <typename T>
-class OPERATOR {
- public:
-  T operator()(const T &a, const T &b) const { return a + b; }
+template <typename T> class OPERATOR {
+  public:
+    T operator()(const T& a, const T& b) const { return a + b; }
 };
 
-template <typename T, typename O = OPERATOR<T>, class G = greater<pair<T, int>>>
+template <typename T,
+          typename O = OPERATOR<T>,
+          class G = std::greater<std::pair<T, int>>>
 class Dijkstra {
- private:
-  int n;
-  vector<vector<pair<int, T>>> edges;
-  O op;
+  private:
+    int n;
+    std::vector<std::vector<std::pair<int, T>>> edges;
+    O op;
 
- public:
-  Dijkstra(const vector<vector<pair<int, T>>> &_edges, O _op = O{})
-      : n(static_cast<int>(_edges.size())), edges(_edges) {}
+  public:
+    Dijkstra(const std::vector<std::vector<std::pair<int, T>>>& _edges)
+        : n(static_cast<int>(_edges.size())), edges(_edges) {}
 
-  T singleDistance(int start, int end, T maximum = numeric_limits<T>::max(),
-                   T minimum = 0, G &&greater = {}) {
-    vector<T> dist(n, maximum);
-    vector<bool> seen(n, false);
-    priority_queue<pair<T, int>, vector<pair<T, int>>, G> pq;
-    dist[start] = minimum;
-    pq.emplace(minimum, start);
-    while (not pq.empty()) {
-      auto [_, curr] = pq.top();
-      pq.pop();
-      if (not seen[curr]) {
-        seen[curr] = true;
-        if (curr == end) return dist[curr];
-        for (auto &&[neighbour, distance] : edges[curr]) {
-          auto newDistance = op(dist[curr], distance);
-          if (greater(make_pair(dist[neighbour], neighbour),
-                      make_pair(newDistance, neighbour))) {
-            dist[neighbour] = newDistance;
-            pq.emplace(newDistance, neighbour);
-          }
+    T singleDistance(int start,
+                     int end,
+                     T maximum = std::numeric_limits<T>::max(),
+                     T minimum = 0,
+                     G&& greater = {}) {
+        std::vector<T> dist(n, maximum);
+        std::vector<bool> seen(n, false);
+        std::priority_queue<std::pair<T, int>, std::vector<std::pair<T, int>>,
+                            G>
+            pq;
+        dist[start] = minimum;
+        pq.emplace(minimum, start);
+        while (not pq.empty()) {
+            auto [_, curr] = pq.top();
+            pq.pop();
+            if (not seen[curr]) {
+                seen[curr] = true;
+                if (curr == end) return dist[curr];
+                for (auto&& [neighbour, distance] : edges[curr]) {
+                    auto newDistance = op(dist[curr], distance);
+                    if (greater(std::make_pair(dist[neighbour], neighbour),
+                                std::make_pair(newDistance, neighbour))) {
+                        dist[neighbour] = newDistance;
+                        pq.emplace(newDistance, neighbour);
+                    }
+                }
+            }
         }
-      }
+        return maximum;
     }
-    return maximum;
-  }
 
-  vector<T> allDistance(int start, T maximum = numeric_limits<T>::max(),
-                        T minimum = 0, G &&greater = {}) {
-    vector<T> dist(n, maximum);
-    vector<bool> seen(n, false);
-    priority_queue<pair<T, int>, vector<pair<T, int>>, G> pq;
-    dist[start] = minimum;
-    pq.emplace(minimum, start);
-    while (not pq.empty()) {
-      auto [_, curr] = pq.top();
-      pq.pop();
-      if (not seen[curr]) {
-        seen[curr] = true;
-        for (auto &&[neighbour, distance] : edges[curr]) {
-          auto newDistance = op(dist[curr], distance);
-          if (greater(make_pair(dist[neighbour], neighbour),
-                      make_pair(newDistance, neighbour))) {
-            dist[neighbour] = newDistance;
-            pq.emplace(newDistance, neighbour);
-          }
+    std::vector<T> allDistance(int start,
+                               T maximum = std::numeric_limits<T>::max(),
+                               T minimum = 0,
+                               G&& greater = {}) {
+        std::vector<T> dist(n, maximum);
+        std::vector<bool> seen(n, false);
+        std::priority_queue<std::pair<T, int>, std::vector<std::pair<T, int>>,
+                            G>
+            pq;
+        dist[start] = minimum;
+        pq.emplace(minimum, start);
+        while (not pq.empty()) {
+            auto [_, curr] = pq.top();
+            pq.pop();
+            if (not seen[curr]) {
+                seen[curr] = true;
+                for (auto&& [neighbour, distance] : edges[curr]) {
+                    auto newDistance = op(dist[curr], distance);
+                    if (greater(std::make_pair(dist[neighbour], neighbour),
+                                std::make_pair(newDistance, neighbour))) {
+                        dist[neighbour] = newDistance;
+                        pq.emplace(newDistance, neighbour);
+                    }
+                }
+            }
         }
-      }
+        return dist;
     }
-    return dist;
-  }
 };
